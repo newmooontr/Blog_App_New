a206271f155ef1dff38e8fd6f192471bc0343bf8
newmoon <newmooon.tr@gmail.com>
2023-03-15 13:02:11 +0300

    added Register

--- main/settings.py
+++ main/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #thirdparty
+    'rest_framework',
+    'rest_framework.authtoken',
+    
+    #myApps
+    'user',
 ]
 
 MIDDLEWARE = [
--- user/views.py
+++ user/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from .serializers import RegisterSerializers
+from django.contrib.auth.models import User
+from rest_framework.generics import CreateAPIView
+from rest_framework.authtoken.models import Token
+
 
 # Create your views here.
+
+
+class RegisterView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializers
+    
+    
+    def create(self, request, *args, **kwargs): # metod override etmek
+        response = super().create(request, *args, **kwargs)
+        token = Token.objects.create(user_id=response.data['id'])
+        response.data['token'] = token.key #gelen dataya token ekleyerek register olan user ı direkt ana sayfaya yönlendirmek.
+        # print(response.data)
+        return response
