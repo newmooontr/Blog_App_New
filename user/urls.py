cad8c78594b17bb23780a54540bbf2a1855ae0b1
newmoon <newmooon.tr@gmail.com>
2023-03-15 13:36:53 +0300

    added Login

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 
 from django.urls import path
 from .views import RegisterView
+from rest_framework.authtoken import views 
 
 urlpatterns = [
  
     path("register/", RegisterView.as_view()),
+    path('login/', views.obtain_auth_token)
 ]
